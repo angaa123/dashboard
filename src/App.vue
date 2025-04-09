c1b0f2770c4fddfa3ce4664a46944c4c22dbb4d6
ankhbayar sumiyabayar <75901266+angaa123@users.noreply.github.com>
2025-04-09 16:39:59 +0800

    hi test
    hi test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import { Button } from "@/components/ui/button";
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Button>Click me</Button>
+    <h1 class="text-3xl font-bold underline text-red-500">Hello World</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
