c91bfe766ecd8d5c0245458e17d07dea8fcd2ef7
ankhbayar sumiyabayar <75901266+angaa123@users.noreply.github.com>
2025-04-09 18:40:01 +0800

    1
    1

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,12 @@ import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
 <template>
-  <div class="flex w-screen h-screen bg-amber-700">
-    <div class="w-1/4">
-      <SidebarProvider>
-        <AppSidebar />
-        <main>
-          <SidebarTrigger />
-          <RouterView />
-        </main>
-      </SidebarProvider>
-    </div>
-    <div class="w-full bg-blue-500">
-      <HelloWorld msg="Hello World" />
-    </div>
-  </div>
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+      <RouterView />
+    </main>
+    <HelloWorld msg="Hello World" />
+  </SidebarProvider>
 </template>
